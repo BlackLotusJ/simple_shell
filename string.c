64c64d3af75dc03b48a7673bda8b2d2807b47153
BlackLotusJ <jerrykiche61@gmail.com>
2023-09-20 21:45:46 +0300

    corrected string.c

--- string.c
+++ string.c
@@ -19,9 +19,9 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - compares two strings.
+ * @s1: String 1
+ * @s2: string 2
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
