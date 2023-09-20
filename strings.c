0b2b8e089cd56525f038e3421415d020ed222c8c
BlackLotusJ <jerrykiche61@gmail.com>
2023-09-20 23:58:43 +0300

    updated strings

--- strings.c
+++ strings.c
@@ -9,6 +9,7 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
 /**
  * _puts - prints a string, followed by a new line, to stdout
  *
@@ -21,6 +22,7 @@ void _puts(char *str)
 
 	_putchar('\n');
 }
+
 /**
  * _strcmp - string to compare
  * @s1: first string
@@ -38,6 +40,7 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (s1[i] - s2[i]);
 }
+
 /**
  * _strcat - concats two arrays
  *
@@ -59,4 +62,37 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[i + j] = '\0';
 	return (dest);
+}
+
+/**
+ *_strcpy - copies a string to another buffer
+ *@source: source to copy from
+ *@dest: destination to copy to
+ *
+ * Return: void
+ */
+void _strcpy(char *source, char *dest)
+{
+	int i = 0;
+
+	for (; source[i] != '\0'; i++)
+		dest[i] = source[i];
+	dest[i] = '\0';
+}
+
+/**
+ *_strlen - counts string length
+ *@string: string to be counted
+ *
+ * Return: length of the string
+ */
+int _strlen(char *string)
+{
+	int len = 0;
+
+	if (string == NULL)
+		return (len);
+	for (; string[len] != '\0'; len++)
+		;
+	return (len);
 }
