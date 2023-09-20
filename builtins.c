012026023b34dd569fd13fa304a7f86b5b3c70df
BlackLotusJ <jerrykiche61@gmail.com>
2023-09-21 01:48:19 +0300

    updated builtins

--- builtins.c
+++ builtins.c
@@ -1,6 +1,35 @@
 #include "shell.h"
 
 /**
+ * check_for_builtin - checks if the command is a builtin
+ * @args: the arguments array
+ *
+ * Return: 1 if command is a builtin, 0 otherwise
+ */
+int check_for_builtin(char **args)
+{
+	if (!args[0])
+		return (0);
+	if (!_strcmp(args[0], "exit"))
+		shell_exit(args);
+	else if (!_strcmp(args[0], "env"))
+		shell_env();
+	else if (!_strcmp(args[0], "setenv"))
+		shell_setenv(args);
+	else if (!_strcmp(args[0], "unsetenv"))
+		shell_unsetenv(args);
+	else if (!_strcmp(args[0], "help"))
+		shell_help();
+	else if (!_strcmp(args[0], "cd"))
+		shell_cd(args);
+	else if (!_strcmp(args[0], "clear"))
+		shell_clear(args);
+	else
+		return (0);
+	return (1);
+}
+
+/**
  * shell_cd - changes the current working directory of the shell
  * @args: array of arguments
  */
@@ -68,4 +97,20 @@ void quit(char **tokenized_command)
 	}
 	else
 		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+}
+
+/**
+ * shell_help - displays help information for built-in commands
+ */
+void shell_help(void)
+{
+	_puts("\nShell Version 1.0.0\n\n");
+	_puts("Usage: ./hsh\n\n");
+	_puts("Shell built-in commands:\n\n");
+	_puts("help\t\tDisplay this help information\n\n");
+	_puts("cd [dir]\tChange the current working directory\n\n");
+	_puts("env\t\tDisplay the environment variables\n\n");
+	_puts("setenv\t\tSet an environment variable\n\n");
+	_puts("unsetenv\tUnset an environment variable\n\n");
+	_puts("exit\t\tExit the shell\n\n");
 }
