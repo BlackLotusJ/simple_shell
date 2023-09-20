182c7e706952b0c88e91c513b98c0500168a5e48
BlackLotusJ <jerrykiche61@gmail.com>
2023-09-21 00:26:21 +0300

    Updated tokenize.c

--- env.c
+++ env.c
@@ -18,6 +18,31 @@ void env(char **tokenized_command __attribute__((unused)))
 	}
 }
 
+/**
+ * _getenv - gets the value of an environment variable
+ * @name: name of the environment variable
+ *
+ * Return: the value of the variable as a string
+ */
+char *_getenv(char *name)
+{
+	char **my_environ;
+	char *pair_ptr;
+	char *name_cpy;
+
+	for (my_environ = environ; *my_environ != NULL; my_environ++)
+	{
+		for (pair_ptr = *my_environ, name_cpy = name;
+		     *pair_ptr == *name_cpy; pair_ptr++, name_cpy++)
+		{
+			if (*pair_ptr == '=')
+				break;
+		}
+		if ((*pair_ptr == '=') && (*name_cpy == '\0'))
+			return (pair_ptr + 1);
+	}
+	return (NULL);
+}
 
 /**
  * quit - exits the shell
--- strings.c
+++ strings.c
@@ -96,4 +96,64 @@ int _strlen(char *string)
 		;
 	return (len);
 }
+/**
+ *_strchr - locates a char in a string
+ *@s: string to be searched
+ *@c: char to be checked
+ *
+ *Return: pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
 
+	for (; s[i] != c && s[i] != '\0'; i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
+
+/**
+ *_strcspn - computes segment of str1 which consists of characters not in str2
+ *@str1: string to be searched
+ *@str2: string to be used
+ *
+ *Return: index at which a char in str1 exists in str2
+ */
+int _strcspn(char *str1, char *str2)
+{
+	int len = 0, i;
+
+	for (i = 0; str1[i] != '\0'; i++)
+	{
+		if (_strchr(str2, str1[i]) != NULL)
+			break;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@str1: string to be searched
+ *@str2: string to be used
+ *
+ *Return: number of bytes in the initial segment of 5 which are part of accept
+ */
+
+int _strspn(char *str1, char *str2)
+{
+	int i = 0;
+	int match = 0;
+
+	while (str1[i] != '\0')
+	{
+		if (_strchr(str2, str1[i]) == NULL)
+			break;
+		match++;
+		i++;
+	}
+	return (match);
+}
