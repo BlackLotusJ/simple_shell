facb756f364022b73ecdd13dc4001627f3a7b52c
Jerry Kiche <66734885+BlackLotusJ@users.noreply.github.com>
2023-09-21 02:14:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -15,7 +15,6 @@ int main(int argc __attribute__((unused)), char **argv)
 {
 	char **commands = NULL;
 	char *line = NULL;
-	char *shell_name = NULL;
 	int status = 0;
 	char **current_command = NULL;
 	int i, type_command = 0;
