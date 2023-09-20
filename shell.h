e66ffcdda61d00adb6caec49036bb9780e6f6d7d
BlackLotusJ <jerrykiche61@gmail.com>
2023-09-20 23:53:59 +0300

    updated strings.c

---
+++ shell.h
@@ -0,0 +1,25 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
+
+/*HELPER FUNCTIONS*/
+int _strlen(char *string);
+void _strcpy(char *source, char *dest);
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+int _strspn(char *, char *);
+int _strcspn(char *, char *);
+char *_strchr(char *, char);
+
+/*MAIN FUNCTIONS*/
+
